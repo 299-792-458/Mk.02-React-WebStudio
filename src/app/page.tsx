55564c9267399b0dd7c9d97ecc26e1298cd8985e
667700996 <hwan@onnuripg.com>
2025-11-22 16:03:52 +0900

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,22 @@ import PlaybookSection from '@/components/home/PlaybookSection';
 import TestimonialsSection from '@/components/home/TestimonialsSection';
 import LogosRibbon from '@/components/home/LogosRibbon';
 import GlobalCta from '@/components/GlobalCta';
+import ShowcaseSection from '@/components/home/ShowcaseSection';
+import ToolkitSection from '@/components/home/ToolkitSection';
+import FaqSection from '@/components/home/FaqSection';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <LogosRibbon />
+      <ShowcaseSection />
       <CapabilitiesSection />
+      <ToolkitSection />
       <CaseStudiesSection />
       <PlaybookSection />
       <TestimonialsSection />
+      <FaqSection />
       <GlobalCta />
       <LatestPosts />
     </main>
