85af57c886e6a6e8cc572213823830374ffc931e
667700996 <hwan@onnuripg.com>
2025-12-12 16:54:10 +0900

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,77 +1,19 @@
-import Hero from '@/components/Hero';
-import LatestPosts from '@/components/LatestPosts';
-import CapabilitiesSection from '@/components/home/CapabilitiesSection';
-import CaseStudiesSection from '@/components/home/CaseStudiesSection';
-import PlaybookSection from '@/components/home/PlaybookSection';
-import TestimonialsSection from '@/components/home/TestimonialsSection';
-import LogosRibbon from '@/components/home/LogosRibbon';
-import GlobalCta from '@/components/GlobalCta';
-import ShowcaseSection from '@/components/home/ShowcaseSection';
-import ToolkitSection from '@/components/home/ToolkitSection';
-import FaqSection from '@/components/home/FaqSection';
-import ModesSection from '@/components/home/ModesSection';
-import ChecklistSection from '@/components/home/ChecklistSection';
-import MomentumTicker from '@/components/home/MomentumTicker';
-import UniversalSystemSection from '@/components/home/UniversalSystemSection';
-import ScrollScenesSection from '@/components/home/ScrollScenesSection';
-import InnovationStackSection from '@/components/home/InnovationStackSection';
-import ShowreelSection from '@/components/home/ShowreelSection';
-import PerformanceWallSection from '@/components/home/PerformanceWallSection';
-import ArchitectureSection from '@/components/home/ArchitectureSection';
-import OmniDeckSection from '@/components/home/OmniDeckSection';
-import LabBurstSection from '@/components/home/LabBurstSection';
-import DistinctionsSection from '@/components/home/DistinctionsSection';
-import CommandPaletteSection from '@/components/home/CommandPaletteSection';
-import AwardOrbitSection from '@/components/home/AwardOrbitSection';
-import KineticMetricsSection from '@/components/home/KineticMetricsSection';
-import SignalRadarSection from '@/components/home/SignalRadarSection';
-import LaunchSchedulerSection from '@/components/home/LaunchSchedulerSection';
-import MotionChoreoSection from '@/components/home/MotionChoreoSection';
-import HyperPlaygroundSection from '@/components/home/HyperPlaygroundSection';
-import AnimationGallerySection from '@/components/home/AnimationGallerySection';
-import DesignForgeSection from '@/components/home/DesignForgeSection';
-import LiveOpsWallSection from '@/components/home/LiveOpsWallSection';
-import ParallaxRunwaySection from '@/components/home/ParallaxRunwaySection';
-import KineticTimelineSection from '@/components/home/KineticTimelineSection';
+import RebootHero from '@/components/home/RebootHero';
+import SignalMarquee from '@/components/home/SignalMarquee';
+import SystemBlueprint from '@/components/home/SystemBlueprint';
+import WorkShowcases from '@/components/home/WorkShowcases';
+import CapabilityMatrix from '@/components/home/CapabilityMatrix';
+import FinalCta from '@/components/home/FinalCta';
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <LogosRibbon />
-      <MomentumTicker />
-      <UniversalSystemSection />
-      <ScrollScenesSection />
-      <OmniDeckSection />
-      <HyperPlaygroundSection />
-      <InnovationStackSection />
-      <LabBurstSection />
-      <CommandPaletteSection />
-      <SignalRadarSection />
-      <LiveOpsWallSection />
-      <ShowreelSection />
-      <KineticMetricsSection />
-      <MotionChoreoSection />
-      <AnimationGallerySection />
-      <DesignForgeSection />
-      <ParallaxRunwaySection />
-      <KineticTimelineSection />
-      <ShowcaseSection />
-      <DistinctionsSection />
-      <LaunchSchedulerSection />
-      <AwardOrbitSection />
-      <ArchitectureSection />
-      <ModesSection />
-      <CapabilitiesSection />
-      <ToolkitSection />
-      <ChecklistSection />
-      <CaseStudiesSection />
-      <PerformanceWallSection />
-      <PlaybookSection />
-      <TestimonialsSection />
-      <FaqSection />
-      <GlobalCta />
-      <LatestPosts />
+      <RebootHero />
+      <SignalMarquee />
+      <SystemBlueprint />
+      <WorkShowcases />
+      <CapabilityMatrix />
+      <FinalCta />
     </main>
   );
 }
