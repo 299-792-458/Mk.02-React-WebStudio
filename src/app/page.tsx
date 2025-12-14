08a942e11a094e35ce47913e2690b914efd71be1
667700996 <hwan@onnuripg.com>
2025-12-15 08:53:38 +0900

    -

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,17 @@ import dynamic from 'next/dynamic';
 import RebootHero from '@/components/home/RebootHero';
 
 const SignalMarquee = dynamic(() => import('@/components/home/SignalMarquee'));
+const RealtimeDashboard = dynamic(() => import('@/components/home/RealtimeDashboard'));
 const SystemBlueprint = dynamic(() => import('@/components/home/SystemBlueprint'));
-const WorkShowcases = dynamic(() => import('@/components/home/WorkShowcases'));
-const CapabilityMatrix = dynamic(() => import('@/components/home/CapabilityMatrix'));
-const FinalCta = dynamic(() => import('@/components/home/FinalCta'));
+
+// ...
 
 export default function Home() {
   return (
     <main>
       <RebootHero />
       <SignalMarquee />
+      <RealtimeDashboard />
       <SystemBlueprint />
       <WorkShowcases />
       <CapabilityMatrix />
